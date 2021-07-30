dc7984deaf7dfd72429e1d8b6dbc658f67f5c849
shubham <saurabhroy543@gmail.com>
2021-07-30 15:03:25 +0530

    Initial commit

---
+++ listen_to_github.py
@@ -0,0 +1,19 @@
+from flask import json,request,Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'welcom guys'
+
+
+@app.route('/github', methods=['POST'])
+def app_gh_message():
+    if request.headers['Content-Type']=='application/json':
+        my_info=json.dumps(request.json)
+        print(my_info)
+        return my_info
+
+
+if __name__=='__main__':
+    app.run(debug=True)
